9a9ac4198f3a950992ff9155c4922a9155eaf74e
wallyson <wallysongames4@gmail.com>
2025-07-13 23:44:03 -0300

    ajustes para o deploy

--- back-end/api/server.js
+++ back-end/api/server.js
@@ -1,6 +1,10 @@
 import express from "express"
 import cors from "cors"
 import { db } from "./connect.js";
+import path from "path";
+
+const __dirname = path.resolve();
+
 
 const app = express();
 const PORT = 3001;
@@ -8,17 +12,23 @@ const PORT = 3001;
 app.use(cors());
 
 
-app.get('/', (request, response) => {
-    response.send("ola pinto")
+app.get('/api/', (request, response) => {
+    response.send("API está fucionando")
 })
 
-app.get('/artists', async(request, response) => {
+app.get('/api/artists', async(request, response) => {
     response.send(await db.collection('artists').find({}).toArray())
 })
 
-app.get('/songs', async(request, response) => {
+app.get('/api/songs', async(request, response) => {
     response.send(await db.collection('songs').find({}).toArray())
 })
+
+app.use(express.static(path.join(__dirname, "../front-end/dist")))
+
+app.use((req, res) => {
+  res.sendFile(path.join(__dirname, "../front-end/dist/index.html"));
+});
 
 app.listen(PORT,() => {
     console.log(`servidor está executando na porta ${PORT}`)
